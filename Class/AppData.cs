ca53ac5c3217d7bb74a99ab23064b4b74fb68069
gFireice <80753741+gFireice@users.noreply.github.com>
2023-01-16 16:12:41 +0300

    Last

--- Class/AppData.cs
+++ Class/AppData.cs
@@ -1,9 +1,5 @@
 ﻿using BarTargu.SqlBase;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BarTargu.Class
 {
--- WindowTar/PageTar/PageCart.xaml.cs
+++ WindowTar/PageTar/PageCart.xaml.cs
@@ -2,20 +2,9 @@
 using BarTargu.SqlBase;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.UI.WebControls;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BarTargu.WindowTar.PageTar
 {
